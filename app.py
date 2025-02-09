d2d00b52386cf95bd2bc8dccb70218031deb6c02
marceloatweb <marcelo@atweb.com.br>
2025-02-09 14:19:32 -0300

    Corrigindo configuração de host e porta para Render

--- app.py
+++ app.py
@@ -67,5 +67,8 @@ def listar_lancamentos():
 
 
 # Iniciar o servidor Flask
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render define a porta automaticamente
+    app.run(host='0.0.0.0', port=port, debug=True)
